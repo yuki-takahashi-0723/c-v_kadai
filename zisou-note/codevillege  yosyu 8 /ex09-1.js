bf05bac9599a478da6de82d1d3b4d87dae9dfefa
yuki-takahashi-0723 <honehonehone0723@gmail.com>
2020-11-27 18:30:31 +0900

    インデントをつけて修正

--- zisou-note/codevillege  yosyu 8 /ex09-1.js
+++ zisou-note/codevillege  yosyu 8 /ex09-1.js
@@ -7,11 +7,11 @@ const removeButton = document.getElementById('remove')
 // イベントリスナの登録
 // 追加
 addButton.addEventListener('click', (event) =>{
-const newItem = document.createElement('li')
-const itemCount = list.children.length + 1
+    const newItem = document.createElement('li')
+    const itemCount = list.children.length + 1
 
-newItem.textContent = '新しいアイテム' +  itemCount
-list.appendChild(newItem)
+    newItem.textContent = '新しいアイテム' +  itemCount
+    list.appendChild(newItem)
 })
 
 
@@ -21,9 +21,9 @@ list.appendChild(newItem)
 // 削除
 
 removeButton.addEventListener('click', (event) =>{
-if(list.children.length===0){
-    alert('削除できるアイテムがありません')
-}else{
-    list.removeChild(list.lastChild)
-}
+    if(list.children.length===0){
+        alert('削除できるアイテムがありません')
+    }else{
+        list.removeChild(list.lastChild)
+    }
 })
--- zisou-note/codevillege  yosyu 8 /ex09-2.js
+++ zisou-note/codevillege  yosyu 8 /ex09-2.js
@@ -7,17 +7,17 @@ const removeButton = document.getElementById('remove')
 // イベントリスナの登録
 // 追加
 addButton.addEventListener('click', (event) =>{
-const newItem = document.createElement('li')
-const newImg = document.createElement('img')
-const newSpan = document.createElement('span')
-const itemCount = list.children.length + 1
+    const newItem = document.createElement('li')
+    const newImg = document.createElement('img')
+    const newSpan = document.createElement('span')
+    const itemCount = list.children.length + 1
 
-newImg.setAttribute('src','http://placehold.it/64x64')
-newImg.setAttribute('alt' , '新しいアイテム' + itemCount)
-newSpan.textContent = '新しいアイテム' +  itemCount
-newItem.appendChild(newImg)
-newItem.appendChild(newSpan)
-list.appendChild(newItem)
+    newImg.setAttribute('src','http://placehold.it/64x64')
+    newImg.setAttribute('alt' , '新しいアイテム' + itemCount)
+    newSpan.textContent = '新しいアイテム' +  itemCount
+    newItem.appendChild(newImg)
+    newItem.appendChild(newSpan)
+    list.appendChild(newItem)
 })
 
 
@@ -27,9 +27,9 @@ list.appendChild(newItem)
 // 削除
 
 removeButton.addEventListener('click', (event) =>{
-if(list.children.length===0){
-    alert('削除できるアイテムがありません')
-}else{
-    list.removeChild(list.lastChild)
-}
+    if(list.children.length===0){
+        alert('削除できるアイテムがありません')
+    }else{
+        list.removeChild(list.lastChild)
+    }
 })
